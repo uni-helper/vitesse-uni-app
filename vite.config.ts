3b87f2753fa1ff769bea02c63fba9a4ac678959c
KeJunMao <kejun1997@gmail.com>
2023-02-13 13:56:09 +0800

    chore: add UniHelperManifest plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import Uni from "@dcloudio/vite-plugin-uni";
+import UniHelperManifest from "@uni-helper/vite-plugin-uni-manifest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    // https://github.com/uni-helper/vite-plugin-uni-manifest
+    UniHelperManifest(),
+    Uni(),
+  ],
 });
