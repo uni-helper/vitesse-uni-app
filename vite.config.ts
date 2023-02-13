1c22c29c45660b1fb7b763c9c662435f00949e7c
KeJunMao <kejun1997@gmail.com>
2023-02-13 14:01:17 +0800

    chore: add AutoImport plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import Uni from "@dcloudio/vite-plugin-uni";
 import UniHelperManifest from "@uni-helper/vite-plugin-uni-manifest";
 import UniHelperPages from "@uni-helper/vite-plugin-uni-pages";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,11 @@ export default defineConfig({
     // https://github.com/uni-helper/vite-plugin-uni-pages
     UniHelperPages(),
     Uni(),
+    AutoImport({
+      imports: ["vue", "pinia", "uni-app"],
+      dts: "src/auto-imports.d.ts",
+      dirs: ["src/composables", "src/stores"],
+      vueTemplate: true,
+    }),
   ],
 });
