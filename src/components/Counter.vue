47aa69a20e0c4315d83a7f11af2144f660c09250
Neil <zguolee@foxmail.com>
2023-06-22 00:16:30 +0800

    chore(deps): update (#10)
    * chore(deps): update
    
    * fix: pnpm patch
    
    * revert(deps): unocss version
    
    * chore: should be work

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -18,6 +18,6 @@ const { count, inc, dec } = useCount()
 
 <style>
 .btn {
-  --at-apply: w-8 h-8 flex items-center justify-center rounded-full bg-teal-600 text-white cursor-pointer;
+  --at-apply: w-8 h-8 flex items-center justify-center rounded-full bg-teal-600 text-white cursor-pointer
 }
 </style>
