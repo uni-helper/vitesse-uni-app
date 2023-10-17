baf4111f8f5f43d78bcc4ee44bf8424f87b470b5
KeJunMao <kejun1997@gmail.com>
2023-10-17 14:16:41 +0800

    feat: use unocss-uni

--- uno.config.ts
+++ uno.config.ts
@@ -1,39 +1,14 @@
-import type {
-  Preset,
-  SourceCodeTransformer,
-} from 'unocss'
-
 import {
-  defineConfig,
-  presetAttributify,
   presetIcons,
-  presetUno,
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
 
-import { presetApplet, presetRemRpx, transformerApplet, transformerAttributify } from 'unocss-applet'
-
-import { isH5, isMp } from '@uni-helper/uni-env'
-
-const presets: Preset[] = []
-const transformers: SourceCodeTransformer[] = []
-const darkMode = isH5 ? 'class' : 'media'
-
-if (isMp) {
-  presets.push(presetApplet({ dark: darkMode }))
-  presets.push(presetRemRpx())
-  transformers.push(transformerAttributify({ ignoreAttributes: ['block', 'fixed'] }))
-  transformers.push(transformerApplet())
-}
-else {
-  presets.push(presetUno({ dark: darkMode }))
-  presets.push(presetAttributify())
-  presets.push(presetRemRpx({ mode: 'rpx2rem' }))
-}
+import { defineConfig, presetUni } from '@uni-helper/unocss-uni'
 
 export default defineConfig({
   presets: [
+    presetUni(),
     presetIcons({
       scale: 1.2,
       warn: true,
@@ -42,14 +17,9 @@ export default defineConfig({
         'vertical-align': 'middle',
       },
     }),
-    ...presets,
   ],
   transformers: [
     transformerDirectives(),
     transformerVariantGroup(),
-    ...transformers,
   ],
-  theme: {
-    preflightRoot: isMp ? ['page,::before,::after'] : undefined,
-  },
 })
