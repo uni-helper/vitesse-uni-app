d8a4788818f66b647f222823cabebd5913a0615e
KeJunMao <kejun1997@gmail.com>
2023-10-17 09:45:11 +0800

    feat: add eslint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ const antfu = require('@antfu/eslint-config').default
 const unocss = require('@unocss/eslint-plugin')
 
 module.exports = antfu(
-  {},
+  {
+    ignores: ['src/manifest.json', 'src/pages.json'],
+  },
   unocss.configs.flat,
 )
