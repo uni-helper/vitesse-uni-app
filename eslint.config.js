2a086ec683540a0944cf2475b269b2b73267aeef
KeJunMao <kejun1997@gmail.com>
2023-11-03 16:23:07 +0800

    chore(eslint): use @uni-helper/eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,7 @@
-const antfu = require('@antfu/eslint-config').default
+const uni = require('@uni-helper/eslint-config')
 const unocss = require('@unocss/eslint-plugin')
 
-module.exports = antfu(
-  {
-    ignores: ['**/manifest.json', '**/pages.json'],
-  },
-  {
-    files: ['**/*.vue'],
-    rules: {
-      'vue/component-name-in-template-casing': [
-        'error',
-        'PascalCase',
-        {
-          registeredComponentsOnly: false,
-          ignores: [
-            'page-meta',
-            'navigator',
-            'web-view',
-          ],
-        },
-      ],
-    },
-  },
+module.exports = uni(
+  {},
   unocss.configs.flat,
 )
