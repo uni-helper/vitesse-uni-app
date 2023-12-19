01fec2508ea2f56df8c37cf5de4285114c5cafb6
chang <areschang114@qq.com>
2023-12-19 17:58:57 +0800

    fix: alipay applet Syntactic compatibility

--- src/composables/useQuery.ts
+++ src/composables/useQuery.ts
@@ -3,7 +3,7 @@ import type { MaybeRef } from '@vueuse/core'
 export function useQuery(key?: MaybeRef<string>) {
   const query = ref<AnyObject>({})
   onLoad((q) => {
-    query.value = q ?? {}
+    query.value = q || {}
   })
   const value = computed(() => (key ? query.value[unref(key)] : null))
   return { query, value }
