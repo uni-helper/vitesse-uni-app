cf67200845f9dfa89a8c4257751812e38ab5f0cd
Rui Wu <wurui.dev@gmail.com>
2024-08-28 14:47:54 +0800

    build: import unocss plugin with async/await

--- vite.config.ts
+++ vite.config.ts
@@ -5,34 +5,37 @@ import UniHelperPages from '@uni-helper/vite-plugin-uni-pages'
 import UniHelperLayouts from '@uni-helper/vite-plugin-uni-layouts'
 import UniHelperComponents from '@uni-helper/vite-plugin-uni-components'
 import AutoImport from 'unplugin-auto-import/vite'
-import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    // https://github.com/uni-helper/vite-plugin-uni-manifest
-    UniHelperManifest(),
-    // https://github.com/uni-helper/vite-plugin-uni-pages
-    UniHelperPages({
-      dts: 'src/uni-pages.d.ts',
-    }),
-    // https://github.com/uni-helper/vite-plugin-uni-layouts
-    UniHelperLayouts(),
-    // https://github.com/uni-helper/vite-plugin-uni-components
-    UniHelperComponents({
-      dts: 'src/components.d.ts',
-      directoryAsNamespace: true,
-    }),
-    Uni(),
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: ['vue', '@vueuse/core', 'uni-app'],
-      dts: 'src/auto-imports.d.ts',
-      dirs: ['src/composables', 'src/stores', 'src/utils'],
-      vueTemplate: true,
-    }),
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    UnoCSS(),
-  ],
-})
+export default async () => {
+  const UnoCSS = (await import('unocss/vite')).default
+
+  return defineConfig({
+    plugins: [
+      // https://github.com/uni-helper/vite-plugin-uni-manifest
+      UniHelperManifest(),
+      // https://github.com/uni-helper/vite-plugin-uni-pages
+      UniHelperPages({
+        dts: 'src/uni-pages.d.ts',
+      }),
+      // https://github.com/uni-helper/vite-plugin-uni-layouts
+      UniHelperLayouts(),
+      // https://github.com/uni-helper/vite-plugin-uni-components
+      UniHelperComponents({
+        dts: 'src/components.d.ts',
+        directoryAsNamespace: true,
+      }),
+      Uni(),
+      // https://github.com/antfu/unplugin-auto-import
+      AutoImport({
+        imports: ['vue', '@vueuse/core', 'uni-app'],
+        dts: 'src/auto-imports.d.ts',
+        dirs: ['src/composables', 'src/stores', 'src/utils'],
+        vueTemplate: true,
+      }),
+      // https://github.com/antfu/unocss
+      // see unocss.config.ts for config
+      UnoCSS(),
+    ],
+  })
+}
