9eb546f8a30bea91ede1878cce48d27e22056503
Panpan <wurui.dev@gmail.com>
2024-11-07 20:07:18 +0800

    chore: eslint fix

--- uno.config.ts
+++ uno.config.ts
@@ -1,11 +1,10 @@
+import { presetUni } from '@uni-helper/unocss-preset-uni'
 import {
   defineConfig,
   presetIcons,
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
-
-import { presetUni } from '@uni-helper/unocss-preset-uni'
 
 export default defineConfig({
   presets: [
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
 import Uni from '@dcloudio/vite-plugin-uni'
+import UniHelperComponents from '@uni-helper/vite-plugin-uni-components'
+import UniHelperLayouts from '@uni-helper/vite-plugin-uni-layouts'
 import UniHelperManifest from '@uni-helper/vite-plugin-uni-manifest'
 import UniHelperPages from '@uni-helper/vite-plugin-uni-pages'
-import UniHelperLayouts from '@uni-helper/vite-plugin-uni-layouts'
-import UniHelperComponents from '@uni-helper/vite-plugin-uni-components'
 import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default async () => {
